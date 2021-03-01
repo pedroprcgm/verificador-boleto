feb05937f0cfac2ddd20d0de13b1b4c05b1d7b93
Pedro Guerreiro <pedro.prcgm@gmail.com>
2021-02-28 23:37:09 -0300

    Ajuste de formatação do arquivo

--- src/app/helpers/pagamento-concessionaria-helper.js
+++ src/app/helpers/pagamento-concessionaria-helper.js
@@ -8,58 +8,58 @@ helper.gerarCodigoBarras = (numero) => {
 
 	codigoBarras = handlerCampoUm(numero, codigoBarras);
 	codigoBarras = handlerCampoDois(numero, codigoBarras);
-    codigoBarras = handlerCampoTres(numero, codigoBarras);
-    codigoBarras = handlerCampoQuatro(numero, codigoBarras);
+	codigoBarras = handlerCampoTres(numero, codigoBarras);
+	codigoBarras = handlerCampoQuatro(numero, codigoBarras);
 
-    let codigoSemDV = codigoBarras.substring(0, 3) + codigoBarras.substring(4);
-    let DVNumero = numero.substring(3, 4);
-    let DVGeral = calcularDVMod10Atualizado(codigoSemDV);
+	let codigoSemDV = codigoBarras.substring(0, 3) + codigoBarras.substring(4);
+	let DVNumero = numero.substring(3, 4);
+	let DVGeral = calcularDVMod10Atualizado(codigoSemDV);
 
-    if (DVNumero != DVGeral) throw 'DV geral inválido!';
+	if (DVNumero != DVGeral) throw "DV geral inválido!";
 
 	return codigoBarras;
 };
 
 handlerCampoUm = (numero, codigoBarras) => {
-    let campoUm = numero.substring(0, 11);
-    let DV = calcularDVMod10Atualizado(campoUm);
+	let campoUm = numero.substring(0, 11);
+	let DV = calcularDVMod10Atualizado(campoUm);
 
-    if (DV != numero.substring(11, 12)) throw 'DV campo um inválido!';
+	if (DV != numero.substring(11, 12)) throw "DV campo um inválido!";
 
-    codigoBarras = stringReplace(codigoBarras, 0, 11, campoUm);
+	codigoBarras = stringReplace(codigoBarras, 0, 11, campoUm);
 
 	return codigoBarras;
 };
 
 handlerCampoDois = (numero, codigoBarras) => {
 	let campoDois = numero.substring(12, 23);
-    let DV = calcularDVMod10Atualizado(campoDois);
-    
-    if (DV != numero.substring(23, 24)) throw 'DV campo dois inválido!';
+	let DV = calcularDVMod10Atualizado(campoDois);
 
-    codigoBarras = stringReplace(codigoBarras, 11, 22, campoDois);
+	if (DV != numero.substring(23, 24)) throw "DV campo dois inválido!";
+
+	codigoBarras = stringReplace(codigoBarras, 11, 22, campoDois);
 
 	return codigoBarras;
 };
 
 handlerCampoTres = (numero, codigoBarras) => {
-    let campoTres = numero.substring(24, 35);
-    let DV = calcularDVMod10Atualizado(campoTres);
+	let campoTres = numero.substring(24, 35);
+	let DV = calcularDVMod10Atualizado(campoTres);
 
-    if (DV != numero.substring(35, 36)) throw 'DV campo três inválido!';
+	if (DV != numero.substring(35, 36)) throw "DV campo três inválido!";
 
-    codigoBarras = stringReplace(codigoBarras, 22, 33, campoTres);
+	codigoBarras = stringReplace(codigoBarras, 22, 33, campoTres);
 
 	return codigoBarras;
 };
 
 handlerCampoQuatro = (numero, codigoBarras) => {
-    let campoQuatro = numero.substring(36, 47);
-    let DV = calcularDVMod10Atualizado(campoQuatro);
+	let campoQuatro = numero.substring(36, 47);
+	let DV = calcularDVMod10Atualizado(campoQuatro);
 
-    if (DV != numero.substring(47, 48)) throw 'DV campo quatro inválido!';
+	if (DV != numero.substring(47, 48)) throw "DV campo quatro inválido!";
 
-    codigoBarras = stringReplace(codigoBarras, 33, 44, campoQuatro);
+	codigoBarras = stringReplace(codigoBarras, 33, 44, campoQuatro);
 
 	return codigoBarras;
 };
